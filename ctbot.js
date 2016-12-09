bad0afab0b5ade9a97d339d551fbaeb87a1557d1
Cody Newman <ctman96@gmail.com>
2016-12-08 18:46:06 -0800

    bind port

--- ctbot.js
+++ ctbot.js
@@ -1,17 +1,21 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client();
+const express = require('express')
+const app = express()
+
 
 // Get authentication details
 try {
 	var AuthDetails = require("./config.json");
 } catch (e){
-	console.log("Could not find config.json, checking for config vars...");
+	console.log("Could not find config.json, checking for process.env");
 	try{
 		var AuthDetails = process.env;
 	} catch (e){
-		console.log("Could not find config-vars, exiting")
+		console.log("Could not find process.env, exiting")
 		process.exit();
 	}
+	console.log("Successfully retrieved process.env")
 }
 
 bot.on('ready', () => {
@@ -27,3 +31,16 @@ bot.on('message', msg => {
 });
 
 bot.login(AuthDetails.TOKEN);
+
+
+
+app.set('port', (AuthDetails.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response){
+	response.send('Pong')
+})
+
+app.listen(app.get('port'), function() {
+	console.log("Running at localhost:"+app.get('port'))
+})
