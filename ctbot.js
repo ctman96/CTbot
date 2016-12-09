d13b1188078ee02b27d84b69c7ab590cd98f752c
Cody Newman <ctman96@gmail.com>
2016-12-08 20:44:31 -0800

    giphy function

--- ctbot.js
+++ ctbot.js
@@ -18,20 +18,56 @@ try {
 	console.log("Successfully retrieved process.env")
 }
 
+var giphy_key = "dc6zaTOxFJmzC"
+
 bot.on('ready', () => {
 	console.log('Logged in successfully');
 });
 
+function get_gif(tags, func){
+	var request = require("request")
+	var q = 'http://api.giphy.com/v1/gifs/random?'+ 'api_key='+giphy_key;
+	if (tags !== null){
+		q += "&tag=" + tags.join('+');
+	}
+	request(q, function(error, response, body){
+		if (error || response.statusCode !== 200) {
+			console.error("gify: Got error: " + body);
+			console.log(error);
+		}
+		else {
+			try{
+				var response = JSON.parse(body);
+				func(responseObj.data.image_url);
+			} catch (e){
+				func(undefined);
+			}
+		}
+	}.bind(this));
+}
+
 bot.on('message', msg => {
+	//List of commands. Make sure to update
 	if(msg.content == '!commands'){
 		msg.channel.sendMessage('Available Commands:');
 		msg.channel.sendMessage('ping : check if bot is alive');
 		msg.channel.sendMessage('!img : send test image');
+		msg.channel.sendMessage('!doge : random doge. giphy test function using fixed tags');
 	}
 
 	if (msg.content === 'ping') msg.reply('Pong!');
 
 	if(msg.content == '!img') msg.channel.sendFile('https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Video-Game-Controller-Icon-IDV-edit.svg/2000px-Video-Game-Controller-Icon-IDV-edit.svg.png');
+
+	if(msg.content == '!doge') {
+		get_gif("doge pupper", function(url){
+			if (url !== undefined)
+				msg.channel.sendMessage(url);
+			else
+				msg.channel.sendMessage("Error retrieving gif. Sorry!");
+		});
+	};
+
 
 
 });
