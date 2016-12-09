23155d35b12513ad4587d641e83d01284074ebc7
Cody Newman <ctman96@gmail.com>
2016-12-09 00:53:01 -0800

    !video command

--- ctbot.js
+++ ctbot.js
@@ -18,7 +18,9 @@ try {
 	console.log("Successfully retrieved process.env")
 }
 
-var giphy_key = "dc6zaTOxFJmzC"
+var giphy_key = "dc6zaTOxFJmzC";
+var youtube_key = "AIzaSyAw5MsLU5Nz9rqYTkPib61PJYL49hLvHx8";
+var youtube_id = "PLLKyePPBYFXE8wmCJEFC_OalwE-CY9X9z";
 
 bot.on('ready', () => {
 	console.log('Logged in successfully');
@@ -46,6 +48,65 @@ function get_gif(tags, func){
 		}
 	}.bind(this));
 }
+//Pull a random video from the playlist, and return a link to it
+function get_video(arg, func){
+	var request = require("request")
+	var q = 'https://www.googleapis.com/youtube/v3/playlistItems?key='+youtube_key+"&part=contentDetails&maxResults=50&playlistId="+youtube_id;
+	console.log("requesting youtube data "+ q);
+	request(q,function(error,response,body){
+		if (error || response.statusCode !== 200) {
+			console.error("video: Got error: " + body);
+			console.log(error);
+		}
+		else {
+			try{
+				var response = JSON.parse(body);
+				var length = response.pageInfo.totalResults;
+				var num = Math.floor(Math.random()*(length)+1)-1;
+				console.log("Found "+length+" videos, getting video "+num);
+				if(num<50){
+					func("https://www.youtube.com/watch?v="+response.items[num].contentDetails.videoId);
+				} else {
+					get_youtube([num-50, response.nextPageToken], function(url){
+						func(url);
+					});
+				}
+			} catch (e){
+				func(undefined);
+			}
+		}
+	}.bind(this));
+}
+//continue get_video by recursively searching through the playlist pages
+function get_youtube(args, func){
+	var request = require("request");
+	var num = args[0];
+	var next_page = args[1];
+	var q = 'https://www.googleapis.com/youtube/v3/playlistItems?key='+youtube_key+"&part=contentDetails&maxResults=50&playlistId="+youtube_id;
+	q+="&pageToken="+next_page;
+	console.log("requesting youtube data "+ q);
+	request(q,function(error,response,body){
+		if (error || response.statusCode !== 200) {
+			console.error("youtube: Got error: " + body);
+			console.log(error);
+		}
+		else {
+			try{
+				var response = JSON.parse(body);
+				if(num<50){
+					func("https://www.youtube.com/watch?v="+response.items[num].contentDetails.videoId);
+				} else {
+					get_youtube([num-50, response.nextPageToken], function(url){
+						func(url);
+					});
+				}
+			} catch (e){
+				func(undefined);
+			}
+		}
+	}.bind(this));
+}
+
 
 //Todo: move old commands over to new parsing
 bot.on('message', msg => {
@@ -55,6 +116,7 @@ bot.on('message', msg => {
 		msg.channel.sendMessage('!ping : check if bot is alive');
 		msg.channel.sendMessage('!img : send test image');
 		msg.channel.sendMessage('!gif [tag1 tag2 ...] : random gif from the tags. if given no arguments, finds random gif');
+		msg.channel.sendMessage('!video : random video from playlist');
 	}
 
 	if (msg.content === '!ping') msg.reply('Pong!');
@@ -67,6 +129,15 @@ bot.on('message', msg => {
 				msg.channel.sendMessage(url);
 			else
 				msg.channel.sendMessage("Error retrieving gif. Sorry!");
+		});
+	}
+
+	if(msg.content == '!video') {
+		get_video("", function(url){
+			if (url !== undefined)
+				msg.channel.sendMessage(url);
+			else
+				msg.channel.sendMessage("Error retrieving video. Sorry!");
 		});
 	}
 
